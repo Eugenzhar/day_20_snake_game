3f79f405d43aff5af548791e3a5aba892ebf167f
Eugene <demontage09@gmail.com>
2022-01-31 14:20:52 +0500

    snake placed in a separate class

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import turtle as t
+import time
+from snake import Snake
 import random
 
 screen = t.Screen()
 screen.screensize(bg="black")
 screen.setup(width=600, height=600)
-
 screen.title("My Snake")
+screen.tracer(0)
 
-start_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in start_positions:
-    tim = t.Turtle(shape="square")
-    tim.color("white")
-    tim.setposition(position)
-    segments.append(tim)
+snake = Snake()
 
 gaming_is_not_over = True
 while gaming_is_not_over:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
 
 
 
