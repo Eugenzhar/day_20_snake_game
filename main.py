7c003da9fe66d649da0a59b9b19d019591037e21
Eugene <demontage09@gmail.com>
2022-01-31 15:31:21 +0500

    snake can be controlled

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ screen.tracer(0)
 
 snake = Snake()
 
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 gaming_is_not_over = True
 while gaming_is_not_over:
     screen.update()
